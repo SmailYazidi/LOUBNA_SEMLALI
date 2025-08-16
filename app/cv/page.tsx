4ea5b87125d9baa181d51b257a77d9ae75ce2280
smailyazidi <smailyazidi@gmail.com>
2025-08-16 17:33:33 +0100

    add header and sidebar

--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -169,14 +169,12 @@ export default function CvPage() {
       </div>
 
       {/* Google Viewer container */}
-<div className="flex justify-center mt-8">
-  <div
-    className={`cv-a4-page ${themeClasses.cardBg} shadow-lg rounded-lg w-full max-w-4xl flex flex-col ${themeClasses.text} 
-                h-[70vh] md:h-[1120px] md:max-w-[794px]`}
-  >
-    <div className="flex flex-col flex-1 mt-8 md:mt-16">
-      {renderContent()}
-    </div>
+<div
+  className={`cv-a4-page ${themeClasses.cardBg} shadow-lg rounded-lg overflow-hidden w-full max-w-4xl flex flex-col ${themeClasses.text} 
+              h-[70vh] md:h-[1120px] md:max-w-[794px]`}
+>
+  <div className="flex flex-col flex-1 mt-8 md:mt-16">
+    {renderContent()}
   </div>
 </div>
 
